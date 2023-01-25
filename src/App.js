08850644bc7b9cdfea17a9d9aae961314be5f447
Luis Guilherme Cipriani <cipriani.luisguilherme@gmail.com>
2023-01-25 16:19:10 -0300

    Altera estilos

--- src/App.js
+++ src/App.js
@@ -1,55 +1,51 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import './estilo.css';
 
 function App() {
 
-const [display, setDisplay] = useState();
+  const [valor, setValor] = useState('');
+  const [operacao, setOperacao] = useState(false);
+  const [resultado, setResultado] = useState(0);
+  const [armazenamento, setArmazenamento] = useState(0);
 
-useEffect(() => {
-  setDisplay('');
-},[]);
-
-function onoff(){
-  if(display === ''){
-    setDisplay(0);
-  }else{
-    setDisplay('');
+  const Botao = (estilo, botao, acao) => {
+    return (
+      <div className={estilo} onClick={acao}>{botao}</div>
+    )
   }
-}
 
   return (
 
     <div className='container'>
       <h1>Calculadora</h1>
-      <img src={require('./assets/Calculadora.png')} className='img' alt='Calculadora' />
-      <a className='display'>{display}</a>
-      <div className='botoes'>
-        <div className='fileira1'>
-          <a className='onoff' onClick={onoff}></a>
+      <div className='calculadora'>
+        <div className='display'>
         </div>
-        <div className='fileira2'>
-          <a className='btn7' onClick={onoff}></a>
-          <a className='btn8' onClick={onoff}></a>
-          <a className='btn9' onClick={onoff}></a>
-          <a className='btnDivisao' onClick={onoff}></a>
-        </div>
-        <div className='fileira3'>
-          <a className='btn4' onClick={onoff}></a>
-          <a className='btn5' onClick={onoff}></a>
-          <a className='btn6' onClick={onoff}></a>
-          <a className='btnMultiplicacao' onClick={onoff}></a>
-        </div>
-        <div className='fileira4'>
-          <a className='btn3' onClick={onoff}></a>
-          <a className='btn2' onClick={onoff}></a>
-          <a className='btn1' onClick={onoff}></a>
-          <a className='btnSubtracao' onClick={onoff}></a>
-        </div>
-        <div className='fileira5'>
-          <a className='btn0' onClick={onoff}></a>
-          <a className='btnPonto' onClick={onoff}></a>
-          <a className='btnIgual' onClick={onoff}></a>
-          <a className='btnSoma' onClick={onoff}></a>
+        <div className='botoes'>
+          <div className='fileira'>
+            {Botao('btn5', '3', '')}
+            {Botao('btn5', '3', '')}
+            {Botao('btn5', '3', '')}
+            {Botao('btn4', '3', '')}
+          </div>
+          <div className='fileira'>
+            {Botao('btn1', '3', '')}
+            {Botao('btn1', '3', '')}
+            {Botao('btn1', '3', '')}
+            {Botao('btn2', '3', '')}
+          </div>
+          <div className='fileira'>
+            {Botao('btn1', '3', '')}
+            {Botao('btn1', '3', '')}
+            {Botao('btn1', '3', '')}
+            {Botao('btn2', '3', '')}
+          </div>
+          <div className='fileira'>
+            {Botao('btn1', '3', '')}
+            {Botao('btn2', '3', '')}
+            {Botao('btn3', '3', '')}
+            {Botao('btn2', '3', '')}
+          </div>
         </div>
       </div>
     </div>
